8cec34c33a07da194d313850e79ab2429c93687a
Guilherme Louro <guilherme-louro@hotmail.com>
2022-07-05 15:42:17 -0300

    feat: add generate token handle

--- src/index.js
+++ src/index.js
@@ -1,31 +1,55 @@
 import { initialize } from '@telnyx/video';
 
 // UI
+document.querySelector("input[name=room-id]").value = process.env.TELNYX_ROOM_ID || ''
+
 const btnConnect = document.getElementById('btn-connect');
+const btnGenerateToken = document.getElementById('btn-generate-token');
 const codeBlock = document.querySelector('code');
+const clientTokenField = document.querySelector('textarea');
+const roomId = document.querySelector("input[name=room-id]").value;
+
+async function generateClientToken() {
+  const response = await fetch(`${process.env.TELNYX_API_URL}/rooms/${roomId}/actions/generate_join_client_token`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.TELNYX_API_KEY}`
+    }
+  }).then((r) => r.json())
+
+  return response.data
+}
 
 async function connectToTheRoom ({ roomId, clientToken, ...rest }){
   const room = await initialize({ roomId, clientToken, ...rest});
 
   room.on('state_changed', function(state) {
     console.log(JSON.stringify(state, null, 2));
+
+    // render UI
     codeBlock.innerHTML += JSON.stringify(state, null, 2);
     codeBlock.innerHTML += '\n';
   });
 
   room.connect().then(function() {
     console.log('You are connected to the room!');
+
+    // render UI
     codeBlock.innerHTML += ':------- You are connected to the room! --------:';
   });
 }
 
-const roomId = "e6e26b76-ca8f-41bd-90b3-719e930d869a";
-const clientToken = "eyJhbGciOiJFZDI1NTE5IiwidHlwIjoiSldUIn0.eyJhdWQiOiJ0ZWxueXhfY2xpZW50X3Rva2VuIiwiZXhwIjoxNjU3MDYxNTQwLCJncmFudHMiOlt7ImFjdGlvbnMiOlsiam9pbiJdLCJyZXNvdXJjZXMiOlsidGVsbnl4OnZpZGVvOnJvb21zOmU2ZTI2Yjc2LWNhOGYtNDFiZC05MGIzLTcxOWU5MzBkODY5YSJdLCJzdWJqZWN0cyI6WyJ0ZWxueXg6dXNlcnM6ODFjNWJjZDAtMWY0OC00YzhhLThhNDItOGY1NWU2NDkzZWE5Il19XSwiZ3JhbnRzX3ZlcnNpb24iOiIxLjAuMCIsImlhdCI6MTY1NzAzMjc0MCwiaXNzIjoidGVsbnl4X2NsaWVudF90b2tlbiIsImp0aSI6IjllYTQ4M2U0LTZmOWUtNDg2Mi04YTFiLTYyMDRkMGE3ZTcyZiIsIm5iZiI6MTY1NzAzMjczOSwic3ViIjoibnVsbCIsInR5cCI6ImFjY2VzcyJ9.sjGtj_tlHRZUm3L1N6MfLNmctlq9VwYGMVFAE1i5VR8nblfQ6edOxn2_iNxhSLBcXeBda8-eTzhBYaK3jd_sBA"
+// Button events
 
-const context = JSON.stringify({ name: 'Bob The Builder', id: 1 })
-
+btnGenerateToken.addEventListener('click', function() {
+  generateClientToken().then(({ token }) => {
+    clientTokenField.value = token
+  })
+})
 
 btnConnect.addEventListener('click', function() {
+  const clientToken = clientTokenField.value
+  const context = JSON.stringify({ name: 'Bob The Builder', id: 1 })
   connectToTheRoom({ roomId, clientToken, context })
 })
 
