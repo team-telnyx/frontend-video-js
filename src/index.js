05c1f76fcd9d982deecb4fba06c9f7dd43e9c1fa
Guilherme Louro <guilherme-louro@hotmail.com>
2022-07-05 12:25:41 -0300

    feat: add prism to style code block

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import { initialize } from '@telnyx/video';
 
+// UI
+const btnConnect = document.getElementById('btn-connect');
+const codeBlock = document.querySelector('code');
+
 async function connectToTheRoom ({ roomId, clientToken, ...rest }){
   const room = await initialize({ roomId, clientToken, ...rest});
-  
+
   room.on('state_changed', function(state) {
-    console.log(JSON.stringify(state, null, 2))
+    console.log(JSON.stringify(state, null, 2));
+    codeBlock.innerHTML += JSON.stringify(state, null, 2);
+    codeBlock.innerHTML += '\n';
   });
-  
+
   room.connect().then(function() {
     console.log('You are connected to the room!');
+    codeBlock.innerHTML += ':------- You are connected to the room! --------:';
   });
 }
 
 const roomId = "e6e26b76-ca8f-41bd-90b3-719e930d869a";
-const clientToken = "eyJhbGciOiJFZDI1NTE5IiwidHlwIjoiSldUIn0.eyJhdWQiOiJ0ZWxueXhfY2xpZW50X3Rva2VuIiwiZXhwIjoxNjU2NTI3NjUxLCJncmFudHMiOlt7ImFjdGlvbnMiOlsiam9pbiJdLCJyZXNvdXJjZXMiOlsidGVsbnl4OnZpZGVvOnJvb21zOmU2ZTI2Yjc2LWNhOGYtNDFiZC05MGIzLTcxOWU5MzBkODY5YSJdLCJzdWJqZWN0cyI6WyJ0ZWxueXg6dXNlcnM6ODFjNWJjZDAtMWY0OC00YzhhLThhNDItOGY1NWU2NDkzZWE5Il19XSwiZ3JhbnRzX3ZlcnNpb24iOiIxLjAuMCIsImlhdCI6MTY1NjUyNzA1MSwiaXNzIjoidGVsbnl4X2NsaWVudF90b2tlbiIsImp0aSI6IjNjNTE1NzYwLTdlZmItNDA3Mi04MzI0LWNkMWFiNGJiOGNmNiIsIm5iZiI6MTY1NjUyNzA1MCwic3ViIjoibnVsbCIsInR5cCI6ImFjY2VzcyJ9.9lon-M04l8BrBhSX0YEH5CKCrd2vq8jeqDKob5DvFibzzg4VF8BfhP8oXYiN6dkqXNK3dquS7zlCvf2lTxdhBg"
+const clientToken = "eyJhbGciOiJFZDI1NTE5IiwidHlwIjoiSldUIn0.eyJhdWQiOiJ0ZWxueXhfY2xpZW50X3Rva2VuIiwiZXhwIjoxNjU3MDYxNTQwLCJncmFudHMiOlt7ImFjdGlvbnMiOlsiam9pbiJdLCJyZXNvdXJjZXMiOlsidGVsbnl4OnZpZGVvOnJvb21zOmU2ZTI2Yjc2LWNhOGYtNDFiZC05MGIzLTcxOWU5MzBkODY5YSJdLCJzdWJqZWN0cyI6WyJ0ZWxueXg6dXNlcnM6ODFjNWJjZDAtMWY0OC00YzhhLThhNDItOGY1NWU2NDkzZWE5Il19XSwiZ3JhbnRzX3ZlcnNpb24iOiIxLjAuMCIsImlhdCI6MTY1NzAzMjc0MCwiaXNzIjoidGVsbnl4X2NsaWVudF90b2tlbiIsImp0aSI6IjllYTQ4M2U0LTZmOWUtNDg2Mi04YTFiLTYyMDRkMGE3ZTcyZiIsIm5iZiI6MTY1NzAzMjczOSwic3ViIjoibnVsbCIsInR5cCI6ImFjY2VzcyJ9.sjGtj_tlHRZUm3L1N6MfLNmctlq9VwYGMVFAE1i5VR8nblfQ6edOxn2_iNxhSLBcXeBda8-eTzhBYaK3jd_sBA"
+
 const context = JSON.stringify({ name: 'Bob The Builder', id: 1 })
 
-connectToTheRoom({ roomId, clientToken, context })
+
+btnConnect.addEventListener('click', function() {
+  connectToTheRoom({ roomId, clientToken, context })
+})
+
+
